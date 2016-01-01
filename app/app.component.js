8e8feae2c72b0efc5bdd5515b288ce294c8cf5b1
yuetsh <517252939@qq.com>
2016-01-01 22:51:55 +0800

    Restart.

--- app/app.component.js
+++ app/app.component.js
@@ -10,7 +10,7 @@ System.register(['angular2/core'], function(exports_1) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
     var core_1;
-    var AppComponent;
+    var AppComponent, HEROES;
     return {
         setters:[
             function (core_1_1) {
@@ -20,34 +20,31 @@ System.register(['angular2/core'], function(exports_1) {
             AppComponent = (function () {
                 function AppComponent() {
                     this.title = 'Tour of Heroes';
-                    this.hero = {
-                        id: 1,
-                        name: 'Windstorm',
-                        details: [
-                            {
-                                address: '黄浦区',
-                                number: 8000,
-                                name: '上海'
-                            },
-                            {
-                                address: '东城区',
-                                number: 8010,
-                                name: '北京'
-                            }
-                        ]
-                    };
+                    this.heros = HEROES;
                 }
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: "\n\t\t<h1>{{title}}</h1>\n\t\t<h2>{{hero.name}} details!</h2>\n        <div>\n            <label>name: </label>\n            <div><input [(ngModel)]=\"hero.name\" placeholder=\"name\"></div>\n        </div>\n        <ol>\n            <li *ngFor=\"#detail of hero.details\">\n                <span class=\"red\">{{detail.number}}</span>\n                <b>{{detail.name}}</b>\n                {{detail.address}}\n            </li>\n        </ol>\n\t",
-                        styles: ["\n        .red {\n            color: red;\n        }\n    "]
+                        template: "\n\t\t<h1>{{title}}</h1>\n\t\t<h2>{{hero.name}} details!</h2>\n        <div>\n            <label>name: </label>\n            <div><input [(ngModel)]=\"hero.name\" placeholder=\"name\"></div>\n        </div>\n\t",
+                        styles: ["\n        \n    "]
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
                 return AppComponent;
             })();
             exports_1("AppComponent", AppComponent);
+            HEROES = [
+                { "id": 11, "name": "Mr. Nice" },
+                { "id": 12, "name": "Narco" },
+                { "id": 13, "name": "Bombasto" },
+                { "id": 14, "name": "Celeritas" },
+                { "id": 15, "name": "Magneta" },
+                { "id": 16, "name": "RubberMan" },
+                { "id": 17, "name": "Dynama" },
+                { "id": 18, "name": "Dr IQ" },
+                { "id": 19, "name": "Magma" },
+                { "id": 20, "name": "Tornado" }
+            ];
         }
     }
 });
--- app/app.component.ts
+++ app/app.component.ts
@@ -4,8 +4,7 @@ import {Component} from 'angular2/core';
 
 interface Hero {
     id: number,
-    name: string,
-    details: [{ address: string, number: number, name: string }]
+    name: string
 }
 
 @Component({
@@ -17,37 +16,26 @@ interface Hero {
             <label>name: </label>
             <div><input [(ngModel)]="hero.name" placeholder="name"></div>
         </div>
-        <ol>
-            <li *ngFor="#detail of hero.details">
-                <span class="red">{{detail.number}}</span>
-                <b>{{detail.name}}</b>
-                {{detail.address}}
-            </li>
-        </ol>
 	`,
     styles: [`
-        .red {
-            color: red;
-        }
+        
     `]
 })
 
 export class AppComponent {
     public title = 'Tour of Heroes';
-    public hero: Hero = {
-        id: 1,
-        name: 'Windstorm',
-        details: [
-            {
-                address: '黄浦区',
-                number: 8000,
-                name: '上海'
-            },
-            {
-                address: '东城区',
-                number: 8010,
-                name: '北京'
-            }
-        ]
-    };
+    public heros = HEROES;
 }
+
+const HEROES: Hero[] = [
+    { "id": 11, "name": "Mr. Nice" },
+    { "id": 12, "name": "Narco" },
+    { "id": 13, "name": "Bombasto" },
+    { "id": 14, "name": "Celeritas" },
+    { "id": 15, "name": "Magneta" },
+    { "id": 16, "name": "RubberMan" },
+    { "id": 17, "name": "Dynama" },
+    { "id": 18, "name": "Dr IQ" },
+    { "id": 19, "name": "Magma" },
+    { "id": 20, "name": "Tornado" }
+];
