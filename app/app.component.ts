a395758cc77bbd5f316734ad6f0422f1efafdb7b
yuetsh <517252939@qq.com>
2016-01-02 14:09:07 +0800

    Add Chapter3.

--- app/app.component.ts
+++ app/app.component.ts
@@ -10,21 +10,80 @@ interface Hero {
 @Component({
     selector: 'my-app',
     template: `
-		<h1>{{title}}</h1>
-		<h2>{{hero.name}} details!</h2>
-        <div>
-            <label>name: </label>
-            <div><input [(ngModel)]="hero.name" placeholder="name"></div>
+        <div *ngIf="selectedHero">
+            <h2>{{selectedHero.name}} details</h2>
+            <div>
+                <label>id: </label> {{selectedHero.id}}
+            </div>
+            <div>
+                <label>name: </label>
+                <input type="text" [(ngModel)]="selectedHero.name" placeholder="name">
+            </div>
         </div>
-	`,
+        <ul class="heroes">
+            <li *ngFor="#hero of heroes" (click)="onSelect(hero)" [class.selected]="hero === selectedHero">
+                <span class="badge">{{hero.id}}</span> {{hero.name}}
+            </li>
+        </ul>
+        `,
     styles: [`
-        
+      .selected {
+        background-color: #CFD8DC !important;
+        color: white;
+      }
+      .heroes {
+        margin: 0 0 2em 0;
+        list-style-type: none;
+        padding: 0;
+        width: 10em;
+      }
+      .heroes li {
+        cursor: pointer;
+        position: relative;
+        left: 0;
+        background-color: #EEE;
+        margin: .5em;
+        padding: .3em 0em;
+        height: 1.6em;
+        border-radius: 4px;
+      }
+      .heroes li.selected:hover {
+        color: white;
+      }
+      .heroes li:hover {
+        color: #607D8B;
+        background-color: #EEE;
+        left: .1em;
+      }
+      .heroes .text {
+        position: relative;
+        top: -3px;
+      }
+      .heroes .badge {
+        display: inline-block;
+        font-size: small;
+        color: white;
+        padding: 0.8em 0.7em 0em 0.7em;
+        background-color: #607D8B;
+        line-height: 1em;
+        position: relative;
+        left: -1px;
+        top: -4px;
+        height: 1.8em;
+        margin-right: .8em;
+        border-radius: 4px 0px 0px 4px;
+      }
     `]
 })
 
 export class AppComponent {
     public title = 'Tour of Heroes';
-    public heros = HEROES;
+    public heroes = HEROES;
+    public selectedHero: Hero;
+
+    onSelect(hero: Hero) {
+        this.selectedHero = hero;
+    }
 }
 
 const HEROES: Hero[] = [
